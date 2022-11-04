b3b68f9b97522ec255d8acf04ccf7e9ccb2d20f6
Dong TRAN <dong.tt@ebisol.vn>
2022-11-04 16:17:45 +0700

    Initial commit

--- middleware/rest-proxy.go
+++ middleware/rest-proxy.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 )
 
-type HandleGetMethod func(http.ResponseWriter, *http.Request)
+type Secure struct{}
 
-func (h HandleGetMethod) Handle(handle func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func (h Secure) Handle(handle func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" || len(r.Header["X"][0]) > 0 {
 			handle(w, r)
